3b98561d8816fb0667f4f01c1d6ff48e06937e14
Ary Sugiarto <arysugiarto10>
2019-08-07 18:16:18 +0700

    commit Screen Selesai

---
+++ app/src/main/java/com/bappedajabar/infobapedda/activity/RegsitrasiActivity.java
@@ -0,0 +1,16 @@
+package com.bappedajabar.infobapedda.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bappedajabar.infobapedda.R;
+
+public class RegsitrasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regsitrasi);
+    }
+}
--- app/src/main/java/com/bappedajabar/infobapedda/fragment/KegiatanFragment.java
+++ app/src/main/java/com/bappedajabar/infobapedda/fragment/KegiatanFragment.java
@@ -1,5 +1,6 @@
 package com.bappedajabar.infobapedda.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,23 +9,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.bappedajabar.infobapedda.R;
+import com.bappedajabar.infobapedda.activity.DetailActivity;
 
 
 public class KegiatanFragment extends Fragment {
 
+    TextView readMore;
     private RecyclerView rvKamar;
     public KegiatanFragment() {
         // Required empty public constructor
     }
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +32,16 @@ public class KegiatanFragment extends Fragment {
         // Inflate the layout for this fragment
 
 //        rvKamar = view.findViewById(R.id.rv_kamar);
+        readMore = view.findViewById(R.id.tv_readmore);
+
+
+        readMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent detail = new Intent(KegiatanFragment.this.getActivity(), DetailActivity.class);
+                startActivity(detail);
+            }
+        });
         return view;
     }
 }
